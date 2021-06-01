379f0b7e955573ff3ac4131f86b7c4686021aff4
akshit-adeshra <jwick4298@gmail.com>
2021-06-01 14:29:39 +0530

    Template Structure done

--- ToDoApp/admin.py
+++ ToDoApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from ToDoApp.models import Todo
 
 
+admin.site.register(Todo)
--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 
+class Todo(models.Model):
+    text = models.CharField(max_length=200)
+    added_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'ToDos'
+
+    def __str__(self):
+        return self.text
